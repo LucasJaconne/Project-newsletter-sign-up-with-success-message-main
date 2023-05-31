ee91f3546c0d817a57f807286367856aef983cff
LucasJaconne <l.jaconne@gmail.com>
2023-05-31 19:38:33 -0300

    atualização do projeto

--- java.js
+++ java.js
@@ -1,7 +1,37 @@
-let b1 = document.querySelector('#b1')
 
-b1.addEventListener('click', clicar)
 
-function clicar(){
-    window.alert('oi,tudo bem ?')
-}
+function validarinput() {
+    let emailInput = document.querySelector('#email_text');
+    let email = emailInput.value;
+    
+    let usuario = email.substring(0, email.indexOf("@"));
+    let dominio = email.substring(email.indexOf("@") + 1);
+  
+    if (
+      usuario.length >= 1 &&
+      dominio.length >= 3 &&
+      usuario.search("@") === -1 &&
+      dominio.search("@") === -1 &&
+      dominio.search(".") !== -1 &&
+      dominio.indexOf(".") >= 1 &&
+      dominio.lastIndexOf(".") < dominio.length - 1
+    ) {
+        let con1 = document.querySelector('#sec1')
+        let con2 = document.querySelector('#sec2')
+        con1.style.display = 'none'
+        con2.style.display = 'block'
+        let p_con2 = document.querySelector('#text2_sec2')
+        p_con2.innerHTML = `A confirmation email has been sent to <strong>${ email}</strong>. Please open it and click the button inside to confirm your subscription.`
+    } else {
+      let invalido = document.querySelector('#email1')
+      
+
+      invalido.innerHTML = `Valid email required`
+      emailInput.style.backgroundColor = '#FDE9E7'
+      emailInput.style.color = '#ff6759'
+      emailInput.style.borderColor = 'red'
+    }
+  }
+   
+
+
